2c2c3b16fa29202e30b780d2dcb4b8187096bc02
Jayyaah <valentines.maitre@gmail.com>
2022-12-13 15:58:40 +0100

    Initial Commit

---
+++ Choume/ChoumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChoumeApp.swift
+//  Choume
+//
+//  Created by Valentine on 13/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct ChoumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
